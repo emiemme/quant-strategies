fb27eec3d2b0312990fcadc1f55ebb16fa193a68
emiemme <emiliano.mazza03@gmail.com>
2024-03-02 16:36:32 +0100

    [quant-strategies] Main update

---
+++ main.py
@@ -0,0 +1,17 @@
+import sys
+sys.path.insert(0, 'strategies/')
+sys.path.insert(0, 'backtest/')
+from strategies import SMA
+from backtest import simple_backtest
+
+
+if __name__ == "__main__":
+    symbol = 'NVDA'
+    start_date = '2024-01-01'
+    end_date = '2024-04-01'
+
+    stock_data, signals = SMA.get_signals(symbol, start_date, end_date)
+
+    portfolio = simple_backtest.backtest_strategy(signals,stock_data,0.01,1000)
+    # Print the portfolio
+    print(portfolio)   
